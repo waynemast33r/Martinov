5f3baaee6e7c673f22393b4350d181154fd1b3a8
Ratmir <ra030405@gmail.com>
2024-01-25 23:14:28 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/ChooseWindowColor.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для ChooseWindowColor.xaml
+    /// </summary>
+    public partial class ChooseWindowColor : Window
+    {
+        string ColorW = "White";
+        public ChooseWindowColor()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton item)
+            {
+                ColorW = item.Content.ToString();
+            }
+        }
+
+        private void Choose_Color_Click(object sender, RoutedEventArgs e)
+        {
+            switch (ColorW)
+            {
+                case "White":
+                    {
+                        App.Current.MainWindow.Background = Brushes.White;
+                        break;
+                    }
+                case "Red":
+                    {
+                        App.Current.MainWindow.Background = Brushes.Red;
+                        break;
+                    }
+                case "Green":
+                    {
+                        App.Current.MainWindow.Background = Brushes.Green;
+                        break;
+                    }
+                case "Blue":
+                    {
+                        App.Current.MainWindow.Background = Brushes.Blue;
+                        break;
+                    }
+                case "Gray":
+                    {
+                        App.Current.MainWindow.Background = Brushes.Gray;
+                        break;
+                    }
+            }
+            Close();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string ChoseColor;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Лабораторная работа №3\nВыполнил студент Cафронов Михаил");
+        }
+        private void Close_Programm(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Mouse_Chenge(object sender, RoutedEventArgs e)
+        {
+            StatusInfo.Text = "Выбранный элемент:";
+            Element.Text = ((FrameworkElement)sender).Name.ToString();
+        }
+        private void Mouse_Leave(object sender, RoutedEventArgs e)
+        {
+            StatusInfo.Text = "";
+            Element.Text = "";
+        }
+
+        private void Choose_Color_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseWindowColor chooseWindowColor = new ChooseWindowColor();
+            chooseWindowColor.Show();
+        }
+    }
+}
